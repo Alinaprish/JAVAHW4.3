a04a627a74e1b92342cec222a3bf119ca90a0a73
Alinaprish <114585491+Alinaprish@users.noreply.github.com>
2022-11-18 18:53:13 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
      public double calculate(int sum, double percent) {
          double MyPercent = percent / 100 / 12;
-        double result = sum * MyPercent + sum * MyPercent / (((1 + MyPercent) * (1 + MyPercent) * (1 + MyPercent) *
+         double result = sum * MyPercent + sum * MyPercent / (((1 + MyPercent) * (1 + MyPercent) * (1 + MyPercent) *
                 (1 + MyPercent) * (1 + MyPercent) * (1 + MyPercent) * (1 + MyPercent) * (1 + MyPercent) *
                 (1 + MyPercent) * (1 + MyPercent) * (1 + MyPercent) * (1 + MyPercent)) - 1);
         return result;
